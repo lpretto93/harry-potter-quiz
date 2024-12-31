7e7ec4a03ef4e349ee2abc44d38d78ae4575ffea
lpretto93 <lpretto@hotmail.it>
2024-12-31 11:17:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click', function() {
+        alert('Welcome to Hogwarts!');
+    });
+});
